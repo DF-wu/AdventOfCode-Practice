675897e9554ad713a482806e754fb0ab9690acfa
dfder <a2470abc@gmail.com>
2023-12-13 19:06:40 +0800

    part1 finished

--- D3/main.go
+++ D3/main.go
@@ -25,11 +25,9 @@ func main() {
 }
 
 func p1(puzzle string) {
-	// puzzle to 2d array
-	puzzles := strings.Split(puzzle, "\n")
-	for k, _ := range puzzles {
-		puzzles[k] = strings.Trim(puzzles[k], "\n")
-	}
+
+	puzzles := strings.Split(puzzle, "\r\n")
+
 	// engine := make([][]string, len(puzzles))
 
 	ans := 0
@@ -40,34 +38,47 @@ func p1(puzzle string) {
 		for col, char := range line {
 			numflagstatus := numflag
 			switch char {
-			case '1', '2', '3', '4', '5', '6', '7', '8', '9':
+			case '0', '1', '2', '3', '4', '5', '6', '7', '8', '9':
 				// number, find left index and right index
 				numflag = true
 			default:
 				numflag = false
 			}
-			if numflagstatus != numflag {
-				// pivot pass over left index or right index
-				if numflag {
-					// left index
-					leftidx = col
-				} else {
-					// right index
-					rightidx = col
-					// verify engine part
-					if enginePartValidator(puzzles, row, leftidx, rightidx) {
-						// find a valid engine part
-						// parse the number and add to sum
-						numberchars := puzzles[row][leftidx:rightidx]
-						// fmt.Println(numberchars)
-						inum, _ := strconv.Atoi(numberchars)
-						ans += inum
-					} else {
-						// str := puzzl es[row][leftidx:rightidx]
-						// fmt.Println(str)
-					}
+			toValidate := false
+			// status handle
+			// a. start to read number:  including end of line
+			// b. reading number excluding last character
+			// c. end reading number.   including end of line
+			// d. read nothing
+			if numflagstatus == false && numflag == true {
+				// a
+				leftidx = col
+				// init. at least one digit
+				rightidx = col
+				if(col == len(line)-1) { toValidate =true }
+			} else if numflagstatus == true && numflag == true {
+				// b
+				rightidx++
+				if(col == len(line)-1) { toValidate =true }
+			} else if numflagstatus == true && numflag == false {
+				// c
+				toValidate = true
+			} else {
+				//d
+				
+			}
+			if( toValidate ){
+				if enginePartValidator(puzzles, row, leftidx, rightidx) {
+					// find a valid engine part
+					// parse the number and add to sum
+					numberchars := puzzles[row][leftidx : rightidx+1]
+					// fmt.Println(numberchars)
+					inum, _ := strconv.Atoi(numberchars)
+					ans += inum
 				}
 			}
+			
+
 		}
 	}
 	fmt.Println(ans)
@@ -94,13 +105,13 @@ func enginePartValidator(puzzles []string, row int, leftidx int, rightidx int) b
 				// out of bound
 				continue
 			}
+
 			if (puzzles[i][j] >= '0' && puzzles[i][j] <= '9') || puzzles[i][j] == '.' {
 				// is a number, pass it
 				// . means nothing
 				// ignore ascii code control codes
 			} else if puzzles[i][j] < 33 {
-				fmt.Printf("!!! %v\n", puzzles[i][j])
-
+				// ignore ascii code control codes
 			} else {
 				// got symbol = is a part of engine
 				// fmt.Printf("the rune is %v | ", string(puzzles[i][j]))

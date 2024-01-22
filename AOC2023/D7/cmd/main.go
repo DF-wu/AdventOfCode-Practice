32e597d9f700d27e766b2506e7f68acc955cfce8
dfder <a2470abc@gmail.com>
2024-01-22 20:38:44 +0800

    d7p2 finished. The code is hideous. Now I knew why I was seen as an others.

--- AOC2023/D7/cmd/main.go
+++ AOC2023/D7/cmd/main.go
@@ -10,8 +10,8 @@ import (
 
 func main() {
 	input := dfmisc.Filereader("input.txt")
-	fmt.Println(p1(input))
-	//fmt.Println(p2(input))
+	//fmt.Println(p1(input))
+	fmt.Println(p2(input))
 	//fmt.Println(input)
 }
 
@@ -54,8 +54,11 @@ func (a sortByHand) Swap(i, j int) {
 }
 func (a sortByHand) Less(i, j int) bool {
 	// determine the strength of each char
+	// p1 without joker type
 	//strengthDict := map[string]int{"A": 14, "K": 13, "Q": 12, "J": 11, "T": 10, "9": 9, "8": 8, "7": 7, "6": 6, "5": 5, "4": 4, "3": 3, "2": 2}
-	strengthDict := map[string]int{"A": 14, "K": 13, "Q": 12, "J": 11, "T": 10, "9": 9, "8": 8, "7": 7, "6": 6, "5": 5, "4": 4, "3": 3, "2": 2}
+	//with  joker tpye
+	strengthDict := map[string]int{"A": 14, "K": 13, "Q": 12, "T": 10, "9": 9, "8": 8, "7": 7, "6": 6, "5": 5, "4": 4, "3": 3, "2": 2, "J": 1}
+
 	//  compare the hand type first !!
 	if a[i].handType < a[j].handType {
 		return true
@@ -81,6 +84,44 @@ func (a sortByHand) Less(i, j int) bool {
 	// only happen in the case of same hand
 	// to maintain the original order -> return false
 	return false
+}
+
+func p2(input string) int {
+	//modeling the input
+	HandCards := []HandCard{}
+	for _, line := range strings.Split(input, "\n") {
+		hand, bid := "", 0
+		fmt.Sscanf(line, "%s %d", &hand, &bid)
+		HandCards = append(HandCards, HandCard{
+			hand:     hand,
+			handType: typeIndicatorWithWildCard(hand),
+			bid:      bid,
+			rank:     0,
+		})
+
+	}
+
+	/*
+		!!!!!!!!!!!!! MY SUPER MAGIC SORT !!!!!!!!!!!!!
+
+		order from low to high
+	*/
+	sort.Sort(sortByHand(HandCards))
+	// update  each handcard rank
+	rankCtr := 1
+	for pos, _ := range HandCards {
+		HandCards[pos].rank = rankCtr
+		rankCtr++
+	}
+
+	fmt.Println(HandCards)
+	// to get the answer, multiply the bid and rank
+	ans := 0
+	for _, handCard := range HandCards {
+		ans = ans + handCard.bid*handCard.rank
+	}
+	return ans
+
 }
 
 func p1(input string) int {
@@ -111,6 +152,7 @@ func p1(input string) int {
 		HandCards[pos].rank = rankCtr
 		rankCtr++
 	}
+
 	// because i use my magic sort. below is not needed
 	//
 	//// count from  FIVEOFAKIND to HIGHCARD
@@ -146,6 +188,132 @@ func p1(input string) int {
 	}
 	return ans
 
+}
+
+func typeIndicatorWithWildCard(hand string) int {
+	// p2 with joker
+	strengthDict := map[string]int{"A": 14, "K": 13, "Q": 12, "T": 10, "9": 9, "8": 8, "7": 7, "6": 6, "5": 5, "4": 4, "3": 3, "2": 2, "J": 1}
+	dict := map[string]int{}
+	for _, str := range hand {
+		//cal freq
+		dict[string(str)]++
+	}
+
+	// check if contains Joker
+	_, isExist := dict["J"]
+	//fmt.Println(indicator)
+	if !isExist {
+		// no joker
+		// follow the same logic as before
+		return TypeMapper(dict)
+
+	} else {
+		// contains joker
+		jokerCtr := dict["J"]
+		delete(dict, "J")
+
+		//Joker always pretent to be the largest card
+		// find largest value slice. ex: 3 3 3 2 2 -> 3 3 3
+		//if there is over 1 max value(same value as max), find the largest card
+
+		// ↓ cause error. Because extra space is added
+		//sortedValues := make([]int, len(dict))
+		sortedValues := []int{}
+		for _, freq := range dict {
+			sortedValues = append(sortedValues, freq)
+		}
+		// from low to high
+		sort.Ints(sortedValues)
+		//init. largest number at least 1
+		if len(sortedValues) == 0 {
+			// JJJJJ
+			return FIVEOFAKIND
+		} else {
+			pivot := len(sortedValues) - 1
+			lastNum := sortedValues[pivot]
+
+			for i := len(sortedValues) - 1; i >= 0; i-- {
+				curr := sortedValues[i]
+				if curr == lastNum {
+					// not changed, keep this element
+				} else {
+					break
+				}
+				pivot--
+			}
+			// compasate the pivot
+			pivot++
+			sliceList := sortedValues[pivot:]
+			// may be 1 or more
+			// if it is 1, then it is the largest value
+			if len(sliceList) == 1 {
+				for k, v := range dict {
+					if sliceList[0] == v {
+						dict[k] = v + jokerCtr
+					}
+				}
+			} else {
+				// more than 1
+				// find the largest card
+				target := sliceList[0] // they should be the same
+
+				//pick a key in dict
+				keys := make([]string, len(dict))
+				for k := range dict {
+					keys = append(keys, k)
+				}
+
+				// init
+				card := keys[0]
+				for k, v := range dict {
+					if target == v {
+						if strengthDict[k] > strengthDict[card] {
+							card = k
+						}
+
+					}
+				}
+				dict[card] = target + jokerCtr
+
+			}
+		}
+
+		return TypeMapper(dict)
+
+	}
+}
+
+func TypeMapper(dict map[string]int) int {
+	sortedValues := []int{}
+	for _, freq := range dict {
+		sortedValues = append(sortedValues, freq)
+	}
+	sort.Ints(sortedValues)
+	indicator := ""
+	// sortd as low to high.  But I like high to low
+	for _, value := range sortedValues {
+		indicator = strconv.Itoa(value) + indicator
+	}
+	// check type
+	switch indicator {
+	case "5":
+		return FIVEOFAKIND
+	case "41":
+		return FOUROFAKIND
+	case "32":
+		return FULLHOUSE
+	case "311":
+		return THREEOFAKIND
+	case "221":
+		return TWOPAIR
+	case "2111":
+		return ONEPAIR
+	case "11111":
+		return HIGHCARD
+	default:
+		//should not be here
+	}
+	return 0
 }
 
 // to determine the hands card type

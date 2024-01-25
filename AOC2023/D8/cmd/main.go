2b0ff559c70ded09a1474c27ee4b12f337b9423c
dfder <a2470abc@gmail.com>
2024-01-25 09:04:31 +0800

    d8p2 finished. Use LCM concept to conquer it.

--- AOC2023/D8/cmd/main.go
+++ AOC2023/D8/cmd/main.go
@@ -6,11 +6,96 @@ import (
 	"strings"
 )
 
+type node struct {
+	L string
+	R string
+}
+
 func main() {
 	input := dfmisc.Filereader("input.txt")
-	fmt.Println(p1(input))
-	//fmt.Println(p2(input))
+	//fmt.Println(p1(input))
+	fmt.Println(p2(input))
 	//fmt.Println(input)
+}
+
+/*
+	LR
+
+	11A = (11B, XXX)
+	11B = (XXX, 11Z)
+	11Z = (11B, XXX)
+	22A = (22B, XXX)
+	22B = (22C, 22C)
+	22C = (22Z, 22Z)
+	22Z = (22B, 22B)
+	XXX = (XXX, XXX)
+*/
+// both simluate or math way is fine. I prefer math way. Find LCM
+func p2(input string) string {
+	recipe := strings.Split(input, "\r\n\r\n")[0]
+	rawMatrix := strings.Split(input, "\r\n\r\n")[1]
+	startingNodes := []string{}
+	matrix := map[string]node{}
+	for _, line := range strings.Split(rawMatrix, "\r\n") {
+		var L, R, entry string
+		line = strings.ReplaceAll(line, ",", "")
+		line = strings.ReplaceAll(line, "(", "")
+		line = strings.ReplaceAll(line, ")", "")
+		fmt.Sscanf(line, "%s = %s %s", &entry, &L, &R)
+		matrix[entry] = node{L, R}
+		if strings.HasSuffix(entry, "A") {
+			startingNodes = append(startingNodes, entry)
+		}
+		fmt.Println(matrix[entry])
+	}
+	fmt.Println(recipe)
+	// find LCM steps for each starting node
+	LCMList := []int{}
+	for _, startingNode := range startingNodes {
+		pivot, ctr := 0, 0
+		curr := startingNode
+		for pivot <= len(recipe) {
+			if pivot == len(recipe) {
+				//reset
+				pivot = 0
+			}
+			if strings.HasSuffix(curr, "Z") {
+				// found
+				break
+			}
+
+			switch string(recipe[pivot]) {
+			case "R":
+				curr = matrix[curr].R
+			case "L":
+				curr = matrix[curr].L
+			default:
+				fmt.Println("error")
+			}
+			ctr++
+			pivot++
+		}
+		LCMList = append(LCMList, ctr)
+	}
+	fmt.Println(LCMList)
+	//LCM = ab / gcd(a,b)
+	LCM := 0
+	for i := 0; i < len(LCMList)-1; i++ {
+		if i == 0 {
+			LCM = LCMList[i] * LCMList[i+1] / gcd(LCMList[i], LCMList[i+1])
+		} else {
+			LCM = LCM * LCMList[i+1] / gcd(LCM, LCMList[i+1])
+		}
+	}
+	fmt.Println(LCM)
+	return ""
+}
+
+func gcd(a, b int) int {
+	if b == 0 {
+		return a
+	}
+	return gcd(b, a%b)
 }
 
 /*
@@ -24,11 +109,6 @@ func main() {
 	GGG = (GGG, GGG)
 	ZZZ = (ZZZ, ZZZ)
 */
-
-type node struct {
-	L string
-	R string
-}
 
 func p1(input string) string {
 	//deserialise input.     Notice: this is windows line ending
